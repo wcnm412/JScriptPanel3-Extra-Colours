f132e915f61ff133ed4134e9b7ba8215046d9676
wcnm412 <136557438+wcnm412@users.noreply.github.com>
2025-11-26 22:30:26 +1100

    Add files via upload

---
+++ common.js
@@ -0,0 +1,481 @@
+// Modified version of common.js from JScript Panel 3 by marc2k3
+
+function _artistFolder(artist) {
+	var folder = folders.artists + utils.ReplaceIllegalChars(artist, true, true);
+	utils.CreateFolder(folder);
+	return folder + '\\';
+}
+
+function _button(x, y, w, h, normal, hover, fn, tiptext) {
+	this.paint = function (gr) {
+		if (this.current.char) {
+			gr.WriteTextSimple(this.current.char, this.font, this.current.colour, this.x, this.y, this.w, this.h, 2, 2);
+		} else if (this.current.img) {
+			_drawImage(gr, this.current.img, this.x, this.y, this.w, this.h, image.full);
+		}
+	}
+
+	this.containsXY = function (x, y) {
+		return x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.h;
+	}
+
+	this.lbtn_up = function (x, y, mask) {
+		if (this.fn) {
+			this.fn(x, y, mask);
+		}
+	}
+
+	this.cs = function (s) {
+		if (s == 'hover') {
+			this.current = this.hover;
+			_tt(this.tiptext);
+		} else {
+			this.current = this.normal;
+		}
+
+		window.RepaintRect(this.x, this.y, this.w, this.h);
+	}
+
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.fn = fn;
+	this.tiptext = tiptext;
+	this.normal = normal;
+	this.hover = hover || normal;
+	this.current = normal;
+
+	if (this.current.char) {
+		this.font = JSON.stringify({Name:'Segoe Fluent Icons',Size:this.h - _scale(10)});
+	}
+}
+
+function _buttons() {
+	this.paint = function (gr) {
+		_.invoke(this.buttons, 'paint', gr);
+	}
+
+	this.move = function (x, y) {
+		var temp_btn = null;
+		_.forEach(this.buttons, function (item, i) {
+			if (item.containsXY(x, y)) {
+				temp_btn = i;
+			}
+		});
+
+		if (this.btn == temp_btn) {
+			return this.btn;
+		}
+
+		if (this.btn) {
+			this.buttons[this.btn].cs('normal');
+		}
+
+		if (temp_btn) {
+			this.buttons[temp_btn].cs('hover');
+		} else {
+			_tt('');
+		}
+
+		this.btn = temp_btn;
+		return this.btn;
+	}
+
+	this.leave = function () {
+		if (this.btn) {
+			_tt('');
+			this.buttons[this.btn].cs('normal');
+		}
+
+		this.btn = null;
+	}
+
+	this.lbtn_up = function (x, y, mask) {
+		if (this.btn) {
+			this.buttons[this.btn].lbtn_up(x, y, mask);
+			return true;
+		}
+
+		return false;
+	}
+
+	this.change_font = function (name) {
+		_.forEach(this.buttons, function (item) {
+			item.font = JSON.stringify({Name:name,Size:item.h - _scale(10)});
+		});
+	}
+
+	this.buttons = {};
+	this.btn = null;
+}
+
+function _clamp(value, min, max) {
+	if (value < min)
+		return min;
+	else if (value > max)
+		return max;
+	else
+		return value;
+}
+
+function _dispose() {
+	_.forEach(arguments, function (item) {
+		if (item) {
+			item.Dispose();
+		}
+	});
+}
+
+function _drawImage(gr, img, dst_x, dst_y, dst_w, dst_h, mode, opacity, border) {
+	if (!img) 
+		return [];
+
+	switch (true) {
+	case (dst_w == dst_h && img.Width == img.Height) || (dst_w == img.Width && dst_h == img.Height):
+	case mode == image.stretch:
+		gr.DrawImage(img, dst_x, dst_y, dst_w, dst_h, 0, 0, img.Width, img.Height, opacity || 1);
+		break;
+	case mode == image.crop:
+	case mode == image.crop_top:
+		if (img.Width / img.Height < dst_w / dst_h) {
+			var src_x = 0;
+			var src_w = img.Width;
+			var src_h = Math.round(dst_h * img.Width / dst_w);
+			var src_y = Math.round((img.Height - src_h) / (mode == image.crop_top ? 4 : 2));
+		} else {
+			var src_y = 0;
+			var src_w = Math.round(dst_w * img.Height / dst_h);
+			var src_h = img.Height;
+			var src_x = Math.round((img.Width - src_w) / 2);
+		}
+
+		gr.DrawImage(img, dst_x, dst_y, dst_w, dst_h, src_x + 3, src_y + 3, src_w - 6, src_h - 6, opacity || 1);
+		break;
+	case mode == image.full:
+	case mode == image.full_top_align:
+	default:
+		var s = Math.min(dst_w / img.Width, dst_h / img.Height);
+		var w = Math.floor(img.Width * s);
+		var h = Math.floor(img.Height * s);
+		dst_x += Math.round((dst_w - w) / 2);
+		dst_y = mode == image.full_top_align ? dst_y : dst_y + Math.round((dst_h - h) / 2);
+		dst_w = w;
+		dst_h = h;
+		gr.DrawImage(img, dst_x, dst_y, dst_w, dst_h, 0, 0, img.Width, img.Height, opacity || 1);
+		break;
+	}
+
+	if (border) {
+		DrawRectangle(gr, dst_x, dst_y, dst_w, dst_h, border);
+	}
+
+	return [dst_x, dst_y, dst_w, dst_h];
+}
+
+function _drawOverlay(gr, x, y, w, h, alpha) {
+	gr.FillRectangle(x, y, w, h, RGBA(0, 0, 0, alpha || 230));
+}
+
+function _explorer(file) {
+	if (utils.IsFile(file)) {
+		utils.Run('explorer', '/select,' + _q(file));
+	}
+}
+
+function _fbEscape(value) {
+	if (typeof value != 'string')
+		return '';
+
+	return value.replace(/'/g, "''").replace(/[\(\)\[\],$]/g, "'$&'");
+}
+
+function _fileExpired(file, period) {
+	return Math.floor(Date.now() / 1000) - utils.GetLastModified(file) > period;
+}
+
+function _firstElement(obj, tag_name) {
+	try {
+		return _.first(obj.getElementsByTagName(tag_name));
+	} catch (e) {}
+
+	return undefined;
+}
+
+function _formatNumber(number, separator) {
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+}
+
+function _getElementsByTagName(value, tag) {
+	doc.open();
+	var div = doc.createElement('div');
+	div.innerHTML = value;
+	var data = div.getElementsByTagName(tag);
+	doc.close();
+	return data;
+}
+
+function _getExt(path) {
+	return path.split('.').pop().toLowerCase();
+}
+
+function _getFiles(folder, exts) {
+	var files = [];
+
+	if (_.isArray(folder)) {
+		folder.forEach(function (item) {
+			Array.prototype.push.apply(files, utils.ListFiles(item).toArray());
+		});
+	} else {
+		files = utils.ListFiles(folder).toArray();
+	}
+
+	if (!exts) {
+		return files;
+	}
+
+	return _.filter(files, function (item) {
+		var ext = _getExt(item);
+		return _.includes(exts, ext);
+	});
+}
+
+function _help(x, y, flags) {
+	var menu = window.CreatePopupMenu();
+	_.forEach(ha_links, function (item, i) {
+		menu.AppendMenuItem(MF_STRING, i + 100, item[0]);
+
+		if (i == 1) {
+			menu.AppendMenuSeparator();
+		}
+	});
+	menu.AppendMenuSeparator();
+	menu.AppendMenuItem(MF_STRING, 1, 'Configure...');
+
+	var idx = menu.TrackPopupMenu(x, y, flags);
+	menu.Dispose();
+
+	switch (idx) {
+	case 0:
+		break;
+	case 1:
+		window.ShowConfigure();
+		break;
+	default:
+		utils.Run(ha_links[idx - 100][1]);
+		break;
+	}
+}
+
+function _isUUID(value) {
+	var re = /^[0-9a-f]{8}-[0-9a-f]{4}-[345][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
+	return re.test(value);
+}
+
+function _jsonParse(value) {
+	try {
+		var data = JSON.parse(value);
+		return data;
+	} catch (e) {
+		return [];
+	}
+}
+
+function _jsonParseFile(file) {
+	return _jsonParse(utils.ReadUTF8(file));
+}
+
+function _lockSize(w, h) {
+	window.MinWidth = window.MaxWidth = w;
+	window.MinHeight = window.MaxHeight = h;
+}
+
+function _menu(x, y, flags) {
+	var menu = window.CreatePopupMenu();
+	var file = new _main_menu_helper('File', 10000, menu);
+	var edit = new _main_menu_helper('Edit', 20000, menu);
+	var view = new _main_menu_helper('View', 30000, menu);
+	var playback = new _main_menu_helper('Playback', 40000, menu);
+	var library = new _main_menu_helper('Library', 50000, menu);
+	var help = new _main_menu_helper('Help', 60000, menu);
+
+	var idx = menu.TrackPopupMenu(x, y, flags);
+	menu.Dispose();
+
+	switch (true) {
+	case idx == 0:
+		break;
+	case idx < 20000:
+		file.mm.ExecuteByID(idx - 10000);
+		break;
+	case idx < 30000:
+		edit.mm.ExecuteByID(idx - 20000);
+		break;
+	case idx < 40000:
+		view.mm.ExecuteByID(idx - 30000);
+		break;
+	case idx < 50000:
+		playback.mm.ExecuteByID(idx - 40000);
+		break;
+	case idx < 60000:
+		library.mm.ExecuteByID(idx - 50000);
+		break;
+	case idx < 70000:
+		help.mm.ExecuteByID(idx - 60000);
+		break;
+	}
+
+	file.mm.Dispose();
+	edit.mm.Dispose();
+	view.mm.Dispose();
+	playback.mm.Dispose();
+	library.mm.Dispose();
+	help.mm.Dispose();
+}
+
+function _main_menu_helper(name, base_id, main_menu) {
+	this.popup = window.CreatePopupMenu();
+	this.mm = fb.CreateMainMenuManager(name);
+	this.mm.BuildMenu(this.popup, base_id);
+	this.popup.AppendTo(main_menu, MF_STRING, name);
+}
+
+function _p(name, default_) {
+	Object.defineProperty(this, _.isBoolean(default_) ? 'enabled' : 'value', {
+		get : function () {
+			return this.val;
+		},
+		set : function (value) {
+			this.val = value;
+			window.SetProperty(this.name, this.val);
+		}
+	});
+
+	this.toggle = function () {
+		this.val = !this.val;
+		window.SetProperty(this.name, this.val);
+	}
+
+	this.name = name;
+	this.default_ = default_;
+	this.val = window.GetProperty(name, default_);
+}
+
+function _q(value) {
+	return '"' + value + '"';
+}
+
+function _save(file, value) {
+	if (utils.WriteTextFile(file, value))
+		return true;
+
+	console.log(N, 'Error saving to ' + file);
+	return false;
+}
+
+function _sb(ch, x, y, w, h, v, fn) {
+	this.paint = function (gr, colour) {
+		if (this.v()) {
+			gr.WriteTextSimple(this.ch, this.font, colour, this.x, this.y, this.w, this.h, 2, 2);
+		}
+	}
+
+	this.containsXY = function (x, y) {
+		return x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.h && this.v();
+	}
+
+	this.move = function (x, y) {
+		if (!this.containsXY(x, y))
+			return false;
+
+		window.SetCursor(IDC_HAND);
+		return true;
+	}
+
+	this.lbtn_up = function (x, y) {
+		if (!this.containsXY(x, y))
+			return false;
+
+		if (this.fn) {
+			this.fn(x, y);
+		}
+
+		return true;
+	}
+
+	this.ch = ch;
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.v = v;
+	this.fn = fn;
+	this.font = JSON.stringify({Name:'Segoe Fluent Icons',Size:h});
+}
+
+function _scale(size) {
+	return Math.round(size * DPI / 72);
+}
+
+function _stringToArray(str, sep) {
+	if (typeof str != 'string' || typeof sep != 'string')
+		return [];
+
+	return str.split(sep).map(function (item) { return item.trim(); }).filter(function (item) { return !item.empty(); });
+}
+
+function _stripTags(value) {
+	doc.open();
+	var div = doc.createElement('div');
+	div.innerHTML = value.toString().replace(/<[Pp][^>]*>/g, '').replace(/<\/[Pp]>/g, '<br>').replace(/\n/g, '<br>');
+	var tmp = div.innerText.trim();
+	doc.close();
+	return tmp;
+}
+
+function _tagged(value) {
+	return value != '' && value != '?';
+}
+
+function _tt(value) {
+	if (tooltip.Text != value) {
+		tooltip.Text = value;
+		tooltip.Activate();
+	}
+}
+
+var doc = new ActiveXObject('htmlfile');
+
+var CRLF = '\r\n';
+var ONE_DAY = 86400;
+var DEFAULT_ARTIST = '$meta(artist,0)';
+var N = window.Name + ':';
+var LM = _scale(5);
+var TM = _scale(22);
+
+var tooltip = window.CreateTooltip('Segoe UI', _scale(12));
+tooltip.SetMaxWidth(800);
+
+var folders = {};
+folders.home = fb.ComponentPath + 'samples\\';
+folders.images = folders.home + 'images\\';
+folders.data = fb.ProfilePath + 'js_data\\';
+folders.artists = folders.data + 'artists\\';
+folders.lastfm = folders.data + 'lastfm\\';
+
+var image = {
+	crop : 0,
+	crop_top : 1,
+	stretch : 2,
+	full : 3,
+	full_top_align : 4,
+};
+
+var ha_links = [
+	['Title Formatting Reference', 'https://wiki.hydrogenaud.io/index.php?title=Foobar2000:Title_Formatting_Reference'],
+	['Query Syntax', 'https://wiki.hydrogenaud.io/index.php?title=Foobar2000:Query_syntax'],
+	['Homepage', 'https://www.foobar2000.org/'],
+	['Components', 'https://www.foobar2000.org/components'],
+	['Wiki', 'https://wiki.hydrogenaud.io/index.php?title=Foobar2000:Foobar2000'],
+	['Forums', 'https://hydrogenaud.io/index.php/board,28.0.html']
+];
---
+++ vu_meter.js
@@ -0,0 +1,547 @@
+// Modified version of vu_meter.js from JScript Panel 3 by marc2k3
+
+var properties = {
+	colour_mode : new _p("2K3.METER.COLOURS.MODE", 1), // 0 UI, 1 custom
+	bar_mode : new _p("2K3.METER.BAR.MODE", 0), // 0 rainbow, 1 solid, 2 gradient
+	custom_background : new _p("2K3.METER.BACKGROUND.COLOUR", RGB(30, 30, 30)),
+	custom_bar : new _p("2K3.METER.BAR.COLOUR", RGB(200, 200, 200)),
+	custom_bar_g1 : new _p("2K3.METER.BAR.G1.COLOUR", RGB(0, 128, 255)),
+	custom_bar_g2 : new _p("2K3.METER.BAR.G2.COLOUR", RGB(255, 50, 10)),
+	custom_peak : new _p("2K3.METER.PEAK.COLOUR"), // no default
+	custom_text : new _p("2K3.METER.TEXT.COLOUR", RGB(240, 240, 240)),
+	meter_style : new _p("2K3.METER.STYLE", 1), // 0: smooth, 1: blocks
+	rms_block_db : new _p("2K3.METER.BLOCK.DB", 0.625),
+	rms_3db : new _p("2K3.METER.AES", false),
+};
+
+// blocks by count style has been removed
+if (properties.meter_style.value > 1) {
+	properties.meter_style.value = 1;
+}
+
+var font_t = CreateFontString("Segoe UI", 8);
+var solid_colour = false;
+var rms_block_dbs = [0.3125, 0.625, 1.25, 2.5];
+var RMS_levels = [], Peak_levels = [], Peak_falldown = [];
+var ChannelNames = [ "FL", "FR", "FC", "LFE", "BL", "BR", "FCL", "FCR", "BC", "SL", "SR", "TC", "TFL", "TFC", "TFR", "TBL", "TBC", "TBR" ];
+var ww = 0, wh = 0, timer_id = 0, rms_db_offset = 0, dBrange = 0;
+
+var channels = {
+	count : 2,
+	config : 0,
+};
+
+var colours = {
+	text : 0,
+	highlight : 0,
+	background : 0,
+	bar : 0,
+};
+
+var brush = {
+	Stops : [],
+	Start : [0, 0], // x and y values
+	End : [0, 0], // x and y values
+};
+
+var rainbow_stops = [
+	[ 0.0, RGB(19, 115, 232) ],
+	[ 0.33, RGB(15, 168, 149) ],
+	[ 0.66, RGB(231, 215, 2) ],
+	[ 1.0, RGB(227, 9, 64) ],
+];
+
+function init() {
+	colourSet = GetNowPlayingColours();
+	dBrange = maxDB - minDB
+
+	update_rms_offset();
+	update_colours();
+
+	if (fb.IsPaused)
+		update_graph();
+	else if (fb.IsPlaying)
+		start_timer();
+	else
+		get_initial_track_info();
+}
+
+function update_rms_offset() {
+	if (properties.rms_3db.enabled) {
+		rms_db_offset = 20 * Math.log(Math.sqrt(2)) / Math.LN10; // 3.01029995663981 dB
+	} else {
+		rms_db_offset = 0;
+	}
+}
+
+function update_colours() {
+	if (properties.colour_mode.value == 0) { // UI
+		if (window.IsDefaultUI) {
+			colours.text = colourSet[1];
+			colours.highlight = colourSet[2];
+			colours.background = colourSet[0];
+		} else {
+			colours.text = colourSet[1];
+			colours.highlight = colourSet[2];
+			colours.background = colourSet[0];
+		}
+
+		solid_colour = colours.text == colours.highlight;
+		colours.peak = colours.text;
+
+		if (solid_colour) {
+			colours.bar = colours.text;
+		} else {
+			brush.Stops = [
+				[0.0, colours.text],
+				[1.0, colours.highlight],
+			]
+
+			colours.bar = JSON.stringify(brush);
+		}
+	} else { // custom
+		colours.background = properties.custom_background.value;
+		colours.text = properties.custom_text.value;
+		colours.peak = properties.custom_peak.value || colours.text;
+
+		if (properties.bar_mode.value == 0) { // rainbow
+			solid_colour = false;
+			brush.Stops = rainbow_stops;
+			colours.bar = JSON.stringify(brush);
+		} else if (properties.bar_mode.value == 1) { // solid colour
+			solid_colour = true;
+			colours.bar = properties.custom_bar.value;
+		} else { // 2 colour gradient
+			solid_colour = false;
+
+			brush.Stops = [
+				[0.0, properties.custom_bar_g1.value],
+				[1.0, properties.custom_bar_g2.value],
+			];
+
+			colours.bar = JSON.stringify(brush);
+		}
+	}
+}
+
+function clear_graph() {
+	for (var c = 0; c < channels.count; ++c) {
+		RMS_levels[c] = 0;
+		Peak_levels[c] = 0;
+		Peak_falldown[c] = 0;
+	}
+
+	window.Repaint();
+}
+
+function update_graph() {
+	var cur_time = fb.PlaybackTime;
+
+	if (cur_time < rms_window)
+		return;
+
+	var chunk = fb.GetAudioChunk(rms_window);
+
+	if (!chunk)
+		return;
+
+	channels.count = chunk.ChannelCount;
+	channels.config = chunk.ChannelConfig;
+	var data = chunk.Data.toArray();
+	var frame_len = chunk.SampleCount;
+
+	if (data && channels.count > 0 && frame_len > 0) {
+		var old_count = Peak_levels.length;
+		RMS_levels.length = channels.count;
+		Peak_levels.length = channels.count;
+		Peak_falldown.length = channels.count;
+
+		if (old_count < channels.count) {
+			for (var c = old_count; c < channels.count; ++c) {
+				Peak_levels[c] = 0;
+				Peak_falldown[c] = 0;
+			}
+		}
+
+		for (var c = 0; c < channels.count; ++c) {
+			var sum = 0, peak = 0;
+
+			for (var i = c; i < data.length; i += channels.count) {
+				var s = Math.abs(data[i]);
+
+				if (s > peak) {
+					peak = s;
+				}
+
+				sum += s * s;
+			}
+
+			RMS_levels[c] = Math.sqrt(sum/frame_len);
+
+			if (peak >= Peak_levels[c]) {
+				Peak_levels[c] = peak;
+				Peak_falldown[c] = 0;
+			} else {
+				if (++Peak_falldown[c] > peak_hold) {
+					Peak_levels[c] *= peak_fall_mul;
+				}
+			}
+		}
+
+		window.Repaint();
+	}
+}
+
+function to_db(num) {
+	return 20 * Math.log(num) / Math.LN10;
+}
+
+function start_timer() {
+	if (!timer_id) {
+		timer_id = window.SetInterval(update_graph, timer_interval);
+	}
+}
+
+function stop_timer() {
+	if (timer_id) {
+		window.ClearInterval(timer_id);
+	}
+
+	timer_id = 0;
+}
+
+function get_initial_track_info() {
+	var handle = fb.GetFocusItem();
+
+	if (!handle)
+		return;
+
+	var info = handle.GetFileInfo();
+
+	var idx = info.InfoFind("channels");
+
+	if (idx >= 0) {
+		channels.count = Number(info.InfoValue(idx));
+	}
+
+	idx = info.InfoFind("WAVEFORMATEXTENSIBLE_CHANNEL_MASK");
+
+	if (idx >= 0) {
+		channels.config = Number(info.InfoValue(idx));
+	}
+
+	info.Dispose();
+	handle.Dispose();
+}
+
+function channel_name(ch) {
+	if (ch < ChannelNames.length) {
+		if (channels.config) {
+			for (var i = 0, idx = 0; i < ChannelNames.length; ++i) {
+				if (channels.config & (1 << i)) {
+					if (idx == ch)
+						return ChannelNames[i];
+
+					idx++;
+				}
+			}
+		} else {
+			return ChannelNames[ch];
+		}
+	}
+
+	return "Ch" + (ch + 1);
+}
+
+function on_colours_changed() {
+	update_colours();
+	window.Repaint();
+}
+
+function on_mouse_rbtn_up(x, y) {
+	var menu = window.CreatePopupMenu();
+	var colour_menu = window.CreatePopupMenu();
+	var bars_menu = window.CreatePopupMenu();
+	var style_menu = window.CreatePopupMenu();
+
+	colour_menu.AppendMenuItem(MF_STRING, 1, 'UI');
+	colour_menu.AppendMenuItem(MF_STRING, 2, 'Custom');
+	colour_menu.CheckMenuRadioItem(1, 2, properties.colour_mode.value + 1);
+
+	if (properties.colour_mode.value == 1) {
+		bars_menu.AppendMenuItem(MF_STRING, 100, 'Rainbow');
+		bars_menu.AppendMenuItem(MF_STRING, 101, 'Solid colour');
+		bars_menu.AppendMenuItem(MF_STRING, 102, 'Gradient');
+		bars_menu.CheckMenuRadioItem(100, 102, properties.bar_mode.value + 100);
+
+		if (properties.bar_mode.value == 1) { // solid colour
+			bars_menu.AppendMenuSeparator();
+			bars_menu.AppendMenuItem(MF_STRING, 110, 'Edit...');
+		} else if (properties.bar_mode.value == 2) { // 2 colour gradient
+			bars_menu.AppendMenuSeparator();
+			bars_menu.AppendMenuItem(MF_STRING, 111, 'Gradient 1...');
+			bars_menu.AppendMenuItem(MF_STRING, 112, 'Gradient 2...');
+		}
+
+		bars_menu.AppendTo(colour_menu, MF_STRING, 'Bars');
+
+		colour_menu.AppendMenuSeparator();
+		colour_menu.AppendMenuItem(MF_STRING, 3, 'Background...');
+		colour_menu.AppendMenuItem(MF_STRING, 4, 'Text...');
+		colour_menu.AppendMenuItem(EnableMenuIf(peak_bar_width > 0), 5, 'Peak...');
+	}
+
+	colour_menu.AppendTo(menu, MF_STRING, 'Colours');
+
+	style_menu.AppendMenuItem(MF_STRING, 10, 'Smooth');
+	style_menu.AppendMenuItem(MF_STRING, 11, 'Blocks');
+	style_menu.CheckMenuRadioItem(10, 11, properties.meter_style.value + 10);
+
+	if (properties.meter_style.value == 1) {
+		style_menu.AppendMenuSeparator();
+		style_menu.AppendMenuItem(MF_GRAYED, 0, 'Block width (dB)');
+
+		rms_block_dbs.forEach(function (item, index) {
+			style_menu.AppendMenuItem(MF_STRING, 20 + index, item);
+		});
+
+		var rms_block_db_index = rms_block_dbs.indexOf(properties.rms_block_db.value);
+		style_menu.CheckMenuRadioItem(20, 20 + rms_block_dbs.length, 20 + rms_block_db_index);
+	}
+
+	style_menu.AppendTo(menu, MF_STRING, 'Meter style');
+
+	menu.AppendMenuSeparator();
+	menu.AppendMenuItem(CheckMenuIf(properties.rms_3db.enabled), 30, 'Use AES +3dB RMS');
+	menu.AppendMenuSeparator();
+	menu.AppendMenuItem(MF_STRING, 50, 'Configure...');
+
+	var idx = menu.TrackPopupMenu(x, y);
+	menu.Dispose();
+	colour_menu.Dispose();
+	style_menu.Dispose();
+	bars_menu.Dispose();
+
+	switch (idx) {
+	case 0:
+		break;
+	case 1:
+	case 2:
+		properties.colour_mode.value = idx -1;
+		update_colours();
+		window.Repaint();
+		break;
+	case 3:
+		var tmp = utils.ColourPicker(properties.custom_background.value);
+
+		if (tmp != properties.custom_background.value) {
+			properties.custom_background.value = tmp;
+			update_colours();
+			window.Repaint();
+		}
+		break;
+	case 4:
+		var tmp = utils.ColourPicker(properties.custom_text.value);
+
+		if (tmp != properties.custom_text.value) {
+			properties.custom_text.value = tmp;
+			update_colours();
+			window.Repaint();
+		}
+		break;
+	case 5:
+		var tmp = utils.ColourPicker(colours.peak);
+
+		if (tmp != colours.peak) {
+			properties.custom_peak.value = tmp;
+			update_colours();
+			window.Repaint();
+		}
+		break;
+	case 10:
+	case 11:
+		properties.meter_style.value = idx - 10;
+		window.Repaint();
+		break;
+	case 20:
+	case 21:
+	case 22:
+	case 23:
+		properties.rms_block_db.value = rms_block_dbs[idx - 20];
+		window.Repaint();
+		break;
+	case 30:
+		properties.rms_3db.toggle();
+		update_rms_offset();
+		window.Repaint();
+		break;
+	case 50:
+		window.ShowConfigure();
+		break;
+	case 100:
+	case 101:
+	case 102:
+		properties.bar_mode.value = idx - 100;
+		update_colours();
+		window.Repaint();
+		break;
+	case 110:
+		var tmp = utils.ColourPicker(properties.custom_bar.value);
+
+		if (tmp != properties.custom_bar.value) {
+			properties.custom_bar.value = tmp;
+			update_colours();
+			window.Repaint();
+		}
+		break;
+	case 111:
+		var tmp = utils.ColourPicker(properties.custom_bar_g1.value);
+
+		if (tmp != properties.custom_bar_g1.value) {
+			properties.custom_bar_g1.value = tmp;
+			update_colours();
+			window.Repaint();
+		}
+		break;
+	case 112:
+		var tmp = utils.ColourPicker(properties.custom_bar_g2.value);
+
+		if (tmp != properties.custom_bar_g2.value) {
+			properties.custom_bar_g2.value = tmp;
+			update_colours();
+			window.Repaint();
+		}
+		break;
+	}
+
+	return true;
+}
+
+function on_paint(gr) {
+	gr.Clear(colours.background);
+
+	if (wh < 1 || ww < 1)
+		return;
+
+	var show_ch_labels = true;
+	var show_db_labels = true;
+	var bar_pad_left = _scale(20);
+	var bar_pad_right = _scale(20);
+	var bar_pad_top = _scale(4);
+	var bar_pad_bottom = _scale(24);
+	var bar_width = ww - bar_pad_left - bar_pad_right;
+	var bar_height = Math.floor((wh - bar_pad_top - bar_pad_bottom) / channels.count);
+
+	// bars are too thin for channel labels, hide them
+	if (bar_height < 12) {
+		show_ch_labels = false;
+
+		// bars are too thin for dB scale too
+		if (bar_height < 4) {
+			show_db_labels = false;
+			bar_pad_left = 0;
+			bar_pad_right = 0;
+			bar_pad_top = 0;
+			bar_pad_bottom = 0;
+			bar_width = ww;
+			bar_height = Math.floor(wh / channels.count);
+		}
+	}
+
+	if (bar_width != brush.End[0]) {
+		brush.End[0] = bar_width;
+
+		if (!solid_colour) {
+			colours.bar = JSON.stringify(brush);
+		}
+	}
+
+	// labels
+	if (show_ch_labels) {
+		for (var c = 0; c < channels.count; ++c) {
+			gr.WriteTextSimple(channel_name(c), font_t, colours.text, 0, bar_pad_top + (bar_height * c), bar_pad_left, bar_height, 2, 2);
+		}
+	}
+
+	if (show_db_labels) {
+		var db_spacing = 5;
+
+		if (dBrange < db_spacing) {
+			db_spacing = 1;
+		}
+
+		if (ww * db_spacing / dBrange < _scale(64)) {
+			db_spacing = ((_scale(64) * dBrange) / ww);
+			db_spacing -= (db_spacing % 5);
+		}
+
+		var y = bar_pad_top + (bar_height * channels.count) + _scale(4);
+		gr.FillRectangle(bar_pad_left, y, bar_width, 1, colours.text);
+
+		for (var i = minDB, j = 0; i <= maxDB; i += db_spacing, j++) {
+			var x = bar_pad_left + (bar_width * j / (dBrange / db_spacing));
+			gr.WriteTextSimple(i + "dB", font_t, colours.text, x - (bar_pad_left / 2), y, _scale(100), wh - y, 0, 2);
+			gr.DrawLine(x, y - _scale(2), x, y + _scale(2), 1, colours.text);
+		}
+	}
+
+	// bars
+	if (properties.meter_style.value == 1) { // block mode
+		var block_count = Math.max(Math.floor(dBrange / properties.rms_block_db.value), 1);
+		var block_width = bar_width / block_count;
+		var block_pad = Math.max(Math.ceil(block_width * 0.05), 1);
+	}
+
+	for (var c = 0; c < channels.count; ++c) {
+		if (RMS_levels[c]) {
+			var rms_db = _clamp(to_db(RMS_levels[c]) + rms_db_offset, minDB, maxDB);
+
+			if (properties.meter_style.value == 0) { // smooth mode
+				var width = Math.round(bar_width * (rms_db - minDB) / dBrange);
+				gr.FillRectangle(bar_pad_left, bar_pad_top + (bar_height * c), width, bar_height - 1, colours.bar);
+			} else { // block mode
+				var blocks = Math.round(block_count * (rms_db - minDB) / dBrange);
+				var width = blocks * block_width;
+				gr.FillRectangle(bar_pad_left, bar_pad_top + (bar_height * c), width, bar_height - 1, colours.bar);
+
+				for (var i = 1; i <= blocks; ++i) {
+					gr.FillRectangle(bar_pad_left - Math.ceil(block_pad / 2) + (i * block_width), bar_pad_top + (bar_height * c), block_pad, bar_height - 1, colours.background);
+				}
+			}
+		}
+
+		if (peak_bar_width > 0 && Peak_levels[c] > 0) {
+			var peak_db = _clamp(to_db(Peak_levels[c]), minDB, maxDB);
+
+			if (peak_db > minDB) {
+				var peak_pos = Math.round(bar_width * (peak_db - minDB) / dBrange);
+				gr.FillRectangle(bar_pad_left + peak_pos - peak_bar_width / 2, bar_pad_top + (bar_height * c), peak_bar_width, bar_height - 1, colours.peak);
+			}
+		}
+	}
+}
+
+function on_playback_new_track() {
+	colourSet = GetNowPlayingColours();
+	colours.text = colourSet[1];
+	colours.highlight = colourSet[2];
+	colours.background = colourSet[0];
+	start_timer();
+}
+
+function on_playback_pause(state) {
+	state ? stop_timer() : start_timer();
+}
+
+function on_playback_stop(reason) {
+	if (reason != 2) {
+		stop_timer();
+	}
+
+	clear_graph();
+}
+
+function on_size() {
+	ww = window.Width;
+	wh = window.Height;
+}
